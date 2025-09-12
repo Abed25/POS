60ab99eed5f73520d29e638b11b99998987e542f
Abed25 <officialwekesa254@gmail.com>
2025-09-12 16:53:33 +0300

    Starts the server, Calls my products APIS as a middleware

---
+++ server/server.mjs
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import productRoutes from "./routes/productRoutes.mjs";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+// Routes
+app.use("/api/products", productRoutes);
+
+app.get("/", (req, res) => {
+  res.send("This  is my backend server");
+});
+
+app.listen(PORT, () =>
+  console.log(`🚀 Server running on http://localhost:${PORT}`)
+);
