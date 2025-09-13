2bb9d2c07bf7c25a1b02dc7805a9bd36a87b029f
Abed25 <officialwekesa254@gmail.com>
2025-09-14 01:24:04 +0300

    Created functions to insert and retrive sales

---
+++ server/models/salesModel.mjs
@@ -0,0 +1,19 @@
+import db from "../config/db.mjs";
+
+export const createSale = async ({ product_id, quantity, total_price }) => {
+  const [result] = await db.query(
+    "INSERT INTO sales (product_id, quantity, total_price) VALUES (?, ?, ?)",
+    [product_id, quantity, total_price]
+  );
+  return { id: result.insertId, product_id, quantity, total_price };
+};
+
+export const getSales = async () => {
+  const [rows] = await db.query("SELECT * FROM sales ORDER BY sale_date DESC");
+  return rows;
+};
+
+export const getSaleById = async (id) => {
+  const [rows] = await db.query("SELECT * FROM sales WHERE id = ?", [id]);
+  return rows[0];
+};
