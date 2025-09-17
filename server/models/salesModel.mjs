cf1beb998cd07bfb6111dc7d73ea77a79d5d3a4d
Abed25 <officialwekesa254@gmail.com>
2025-09-18 02:50:08 +0300

    Link sales to product to the current login cashier/admin

--- server/models/salesModel.mjs
+++ server/models/salesModel.mjs
@@ -1,11 +1,17 @@
 import db from "../config/db.mjs";
 
-export const createSale = async ({ product_id, quantity, total_price }) => {
+export const createSale = async ({
+  product_id,
+  quantity,
+  total_price,
+  user_id,
+}) => {
   const [result] = await db.query(
-    "INSERT INTO sales (product_id, quantity, total_price) VALUES (?, ?, ?)",
-    [product_id, quantity, total_price]
+    "INSERT INTO sales (product_id, quantity, total_price, user_id) VALUES (?, ?, ?, ?)",
+    [product_id, quantity, total_price, user_id]
   );
-  return { id: result.insertId, product_id, quantity, total_price };
+
+  return { id: result.insertId, product_id, quantity, total_price, user_id };
 };
 
 export const getSales = async () => {
@@ -23,6 +29,17 @@ export const getSalesByDateRange = async (from, to) => {
   const [rows] = await db.query(
     "SELECT * FROM sales WHERE sale_date BETWEEN ? AND ? ORDER BY sale_date ASC",
     [from, to]
+  );
+  return rows;
+};
+
+// Get sales by cashier (user_id)
+export const getSalesByUser = async (user_id) => {
+  const [rows] = await db.query(
+    "SELECT s.id, s.product_id, p.name AS product_name, s.quantity, s.total_price, s.sale_date " +
+      "FROM sales s LEFT JOIN  products p ON s.product_id = p.id " +
+      "WHERE s.user_id = ? ORDER BY s.sale_date DESC",
+    [user_id]
   );
   return rows;
 };
