6a1d719aad3ebaf11df95cc2fb67109447057b21
Abed25 <officialwekesa254@gmail.com>
2025-09-18 02:54:38 +0300

    Add a static router on top of dynamic route

--- server/routes/salesRoutes.mjs
+++ server/routes/salesRoutes.mjs
@@ -4,13 +4,17 @@ import {
   getAllSales,
   getSingleSale,
   getSalesInRange,
+  getMySales,
 } from "../controllers/salesController.mjs";
 import { protect, authorize } from "../middleware/authMiddleware.mjs";
 
 const router = express.Router();
 
 router.post("/", protect, authorize("cashier", "admin"), addSale); // Record a new sale
+// Cashier gets only their sales
+router.get("/my", protect, authorize("cashier"), getMySales);
 router.get("/", protect, authorize("admin"), getAllSales); // Get all sales
 router.get("/:id", protect, authorize("cashier", "admin"), getSingleSale); // Get one sale
-router.get("/range", protect, authorize("cashier", "admin"), getSalesInRange); // Get sales in range
+router.get("/range", protect, authorize("admin"), getSalesInRange); // Get sales in range
+
 export default router;
